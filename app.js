df8f0b63e456f4e6cd6c200da1deaa1ebafd4001
zflan <lanzf2011@gmail.com>
2016-11-09 09:47:43 +0800

    init app

---
+++ app.js
@@ -0,0 +1,43 @@
+//app.js
+App({
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData:{
+    userInfo:null
+  },
+  doRequest:function(cb){
+    wx.request({
+      url: 'https://www.woidai.cn/api/videos', 
+      data: {
+      },
+      header: {
+          'Content-Type': 'application/json'
+      },
+      success: function(res) {
+        cb(res)
+      }
+    })
+  }
+})
